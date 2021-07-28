33d518a91b195b4ba5506d58b750600d8fd17ce9
Manali - <manalikumari121@gmail.com>
2021-07-29 00:49:00 +0530

    my first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'manali',
+        password: 'manali',
+        database: 'face_recognition'
+    }
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => { res.send('Success'); })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+
+
+
+app.listen(5000, () => {
+    console.log('App is running on port 5000');
+})
