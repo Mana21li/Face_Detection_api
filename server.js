316b859c339a2044635492e8274fe974b28354a7
Manali - <manalikumari121@gmail.com>
2021-07-29 01:33:52 +0530

    2nd Commit

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 
 
-app.listen(5000, () => {
-    console.log('App is running on port 5000');
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`App is running on port ${process.env.PORT}`);
 })
