46a89c78b3ae09d529ba33a7ae9d251b0af58bfa
Manali - <manalikumari121@gmail.com>
2021-08-01 02:19:35 +0530

    updating database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-shaped-52266',
         user: 'manali',
         password: 'manali',
         database: 'face_recognition'
